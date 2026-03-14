601d0810fd00b53898421cfc2efc6ecf868b99b9
FlutterJivi <www.imranbhatti199712@gmail.com>
2026-03-14 19:49:36 +0530

    done

--- src/Abstraction/Abstraction3.java
+++ src/Abstraction/Abstraction3.java
@@ -3,7 +3,7 @@ package Abstraction;
 rule 1 -> abstract class ka object nhi banata
 rule 2 -> abstract class mein abstract method ho bhi sakta hai nahi bhi.
 rule 3 -> agar class mein ek bhi abstract method hai toh pura class abstract karna padega
-rule 4 -> Subclass ko sare abstract methods implement karne padte hain agar ek chose
+rule 4 -> Subclass ko sare abstract methods implement karne padte hain agar ek chose nhi kiya toh error dega
 rule 5 -> Abstract class mai Constructor ho sakta hai wo baat alag hai ki hum object nhi bana sakte par super() se call ho jata hai
 rule 6 -> abstract class mein static methods ho sakte hai
 rule 7 -> Abstract methods mein instance variables ho sakte hain
@@ -19,9 +19,12 @@ class Animal2 extends Animal1{
 
     }
 
-    void sleep(){}
+    @Override
+    void sleep() {
+
+    }
 }
-public class Abstraction2 {
+public class Abstraction3 {
     public static void main(String[] args) {
 
     }
